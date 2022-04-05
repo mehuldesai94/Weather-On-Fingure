6ac48759fc12af1e23d9f7086176f5433bcb2417
Mehul Desai <desaimehul94@gmail.com>
2022-04-04 20:28:58 -0400

    API call working but need to set env variable.

---
+++ public/config.js
@@ -0,0 +1,3 @@
+REACT_APP_API_URL = 'https://api.openweathermap.org/data/2.5'
+REACT_APP_API_KEY = "3a289691966150f9473867d42eeca1dd"
+REACT_APP_ICON_URL = 'https://openweathermap.org/img/w'
--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&APPID=3a289691966150f9473867d42eeca1dd`)
+      .then(response => response.json() )
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
+
+
   return (
     <div className="App">
-      
+
     </div>
   );
 }
